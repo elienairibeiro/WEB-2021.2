5318ffaa43a23fb12fcebf9df80ff2cfad492aaa
Elienai Ribeiro <elienairibeiro.rc@gmail.com>
2021-10-03 17:20:42 -0300

    Projeto React

---
+++ introducao/src/components/elienai.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function elienai(){
+    return <div>
+            <p>Nome Completo: Elienai Ribeiro da Silva</p>
+
+            <p>Idade: 22 anos</p>
+
+            <p>Curso: Engenharia de Software</p>
+
+            <p>Cidade de Origem: Itaitinga</p>        
+            
+            <p>E-mail usado na disciplina: elienairibeiro@alu.ufc.br</p>
+        </div>
+}
+
+export default elienai
---
+++ introducao/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Elienai from './components/elienai'
+
+const root = document.getElementById('root')
+ReactDOM.render(<Elienai/>, root)
